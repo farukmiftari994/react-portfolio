be16af9758b502fd2f75009c0ab1aa5dbd16a208
Faruk Miftari <farukmiftari994@gmail.com>
2024-05-19 19:39:54 +0200

    Replaced Main component with Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Main from "./Components/Main";
+import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 
 const App = () => {
@@ -8,7 +8,7 @@ const App = () => {
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className="container mx-auto px-8">
         <Navbar />
-        <Main />
+        <Hero />
       </div>
     </div>
   );
